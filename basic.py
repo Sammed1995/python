70271c814b8e7fc943349b88aeaaea0928a3e9ba
Sammed  M Alagoudar <114852991+Sammed1995@users.noreply.github.com>
2025-08-30 16:51:05 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,25 @@
+print('Hello, World!' + str(20) + " good to see you") # This is a comment
+
+print(f"20 days in {30} minutes") # f-string for formatted output
+
+# Data Types and Operations
+
+print (type(5)) # int
+print (type(5.0))  # float
+print (type("Hello")) # str
+print (type(True)) # bool
+
+print (5 / 2) # Division
+print(5 + 3) # Addition
+print (5 - 3) # Subtraction
+print (5 * 2) # Multiplication
+print (5 // 2) # Floor Division
+print (5 % 2) # Modulus
+print (5 ** 2) # Exponentiation
+print (5 + 3 * 2) # Order of Operations
+print ((5 + 3) * 2) # Parentheses
+
+minute = 20 * 24 * 60
+print("the minutes in a 20 day is", minute)
+
+# the minutes in a 20 day is 28800
